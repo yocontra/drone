14a81e463a65c765a705fa45aaba8e8522618f46
Contra <contra@maricopa.edu>
2012-10-30 23:48:17 -0700

    bin files

--- onboard/build.sh
+++ onboard/build.sh
@@ -4,6 +4,5 @@ git clone git://github.com/felixge/node-cross-compiler.git
 cd ./node-cross-compiler
 vagrant up
 vagrant ssh -c "cd cross-compiler && ./setup-vm.sh && make ardrone2"
-mkdir ../bin
-cp ./node/build/bin/node ../bin
-#cp ./node/build/bin/npm ../bin
+mkdir -p ../build
+cp -R ./build/* ../build
