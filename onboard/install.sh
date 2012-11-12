4f72383b20cb693acd1224cd102071b36060d38a
Contra <contra@maricopa.edu>
2012-11-12 14:14:34 -0700

    clean

--- onboard/install.sh
+++
@@ -1,8 +0,0 @@
-# copy built node to drone over ftp
-# telnet in and set shit up
-cd ./onboard
-ftp -u ftp://anonymous:anonymous@192.168.1.1/node.tar.gz ./node.tar.gz
-
-{ echo "cd /data/video && tar xvzf node.tar.gz && mv build/bin/node /bin && rm -rf node.tar.gz && rm -rf build && exit"; sleep 1; } | telnet 192.168.1.1
-
-echo "Node has been installed on the drone!"
